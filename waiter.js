f7761f7d6e0b2a5e869ae08e43ddb9199e6ed9ee
Nikola Stamatovic <stamatmail@gmail.com>
2019-12-26 17:49:38 +0100

    Update waiter.js

--- waiter.js
+++ waiter.js
@@ -1,13 +1,13 @@
 function waiter(o, flag) {
 	var defaults = {
-		selector: null, //string - [MANDATORY] DOM element you wait for to appear or a UID if you wait for anything else
+		selector: null, //string - [MANDATORY] DOM element you wait for to appear
 		iterations: null, //int - how many times to try, if blank then infinite untill found
 		success: null, //function - callback on success
 		fail: null, //function - callback on fail
-		timeout: 100, //int - in ms duration of each try
+		interval: 100, //int - in ms duration of each try
 		events: true, //bool - Fire DOM events; name of event === 'waiter-success: ' + selector || 'waiter-fail: ' + selector,
-		condition: function(selector) { //default compare function checking the appearance of DOM by selector. Change if you wait for something custom...
-			var elem = document.querySelectorAll(selector);
+		condition: function(selector) {
+			var elem = document.querySelectorAll(o.selector);
 			return elem.length;
 		}
 	}
@@ -31,7 +31,8 @@ function waiter(o, flag) {
 		window.__waiter_iteration_index[o.selector]++;
 	}
 
-	var valid = o.condition(o.selector);
+	var iter = o.iterations ? window.__waiter_iteration_index[o.selector] : undefined;
+	var valid = o.condition(o, iter);
 
 	var trigger = function(prefix, suffix) {
 		var e = document.createEvent('Event');
@@ -45,20 +46,22 @@ function waiter(o, flag) {
 				trigger('waiter-fail: ', o.selector);
 			}
 			if (o.fail) {
-				o.fail();
+				o.fail(o);
 			}
+
+			window.__waiter_iteration_index[o.selector] = 0;
 			return;
 		}
 
 		setTimeout(function(){
 			waiter(o, true);
-		}, o.timeout);
+		}, o.interval);
 	} else {
 		if (o.events) {
 			trigger('waiter-success: ', o.selector);
 		}
 		if (o.success) {
-			o.success();
+			o.success(o);
 		}
 	}
 }
